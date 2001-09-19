2baa078331afe3af43d8e20bfcb43f143fa14199
Jacek Konieczny <jajcus@pld-linux.org>
2001-09-19 14:37:36 +0000

    - echo anything to stdout only if it is a tty
    Changed files:
        console.sh -> 1.9

--- console.sh
+++ console.sh
@@ -7,7 +7,7 @@ case $TERM in
 		if [ "$CONSOLEMAP" != "" ]; then
 			# Switch the G0 charset map from the default ISO-8859-1
 			# to the user-defined map (loaded with consolefonts)
-			if [ -w /proc/$$/fd/0 ]; then
+			if [ -w /proc/$$/fd/0 -a -t 0]; then
 				echo -n -e '\033(K' > /proc/$$/fd/0
 			else
 				echo -n -e '\033(K' > /dev/tty
