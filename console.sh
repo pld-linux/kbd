fa948679082a27e981922d090aa474c5286f1e68
Paweł Sikora <pluto@pld-linux.org>
2005-09-18 23:38:52 +0000

    - start unicode for utf8 locales.
    Changed files:
        console.sh -> 1.12

--- console.sh
+++ console.sh
@@ -3,7 +3,14 @@ if (tty -s); then
   linux)
  	if [ -f /etc/sysconfig/console ]; then
 		. /etc/sysconfig/console
-	
+
+		[ -f /etc/sysconfig/i18n ] && . /etc/sysconfig/i18n
+		case "$LANG" in
+			*.utf8|*.UTF-8)
+				[ -x /bin/unicode_start ] && /sbin/consoletype fg && /bin/unicode_start
+			;;
+		esac
+
 		if [ "$CONSOLEMAP" != "" ]; then
 			# Switch the G0 charset map from the default ISO-8859-1
 			# to the user-defined map (loaded with consolefonts)
